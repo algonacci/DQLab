102b3ed12946b6d02d54ad6de7aedfe04dafc633
algonacci <86970816+algonacci@users.noreply.github.com>
2021-08-02 22:10:08 +0700

    Add files via upload

---
+++ Python/test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = pd.read_csv("https://storage.googleapis.com/data_science_training/Data.csv")
+print(data)
---
+++ R/Membuat Data Frame Sederhana.R
@@ -0,0 +1,22 @@
+#Membuat dua variable vector
+fakultas <- c("Bisnis", "D3 Perhotelan", "ICT", "Ilmu Komunikasi", "Seni dan Desain")
+jumlah_mahasiswa <- c(260, 28, 284, 465, 735)
+
+#Membuat data frame dari kedua vector di atas
+info_mahasiswa <- data.frame(fakultas, jumlah_mahasiswa)
+
+#Melihat isi data frame
+info_mahasiswa
+
+#Buat vector baru sebagai representasi akreditasi
+akreditasi <- c("A", "A", "B", "A", "A")
+
+#Buat data frame dari ketiga vector di atas
+info_mahasiswa <- data.frame(fakultas, jumlah_mahasiswa, akreditasi)
+info_mahasiswa
+
+#Menampilkan kolom jumlah_mahasiswa
+info_mahasiswa$jumlah_mahasiswa
+
+#Menampilkan kolom fakultas
+info_mahasiswa$fakultas
