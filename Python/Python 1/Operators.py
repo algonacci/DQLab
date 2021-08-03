6da97e12784ea9e051636ac9f39b303d927043ae
algonacci <86970816+algonacci@users.noreply.github.com>
2021-08-03 23:47:58 +0700

    Add files via upload

---
+++ Python/Python 1/Operators.py
@@ -0,0 +1,42 @@
+'''
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+'''
+
+
+
+### STUDI KASUS ###
+
+# Kode awal
+total_harga = 150000
+potongan_harga = 0.3
+pajak = 0.1 # pajak dalam persen ~ 10%
+harga_bayar = 1 - 0.3 # baris pertama
+harga_bayar *= total_harga # baris kedua
+pajak_bayar = pajak * harga_bayar # baris ketiga
+harga_bayar += pajak_bayar # baris ke-4
+print("Kode awal - harga_bayar=", harga_bayar)
+
+# Penyederhanaan baris kode dengan menerapkan prioritas operator
+total_harga = 150000
+potongan_harga = 0.3
+pajak = 0.1 # pajak dalam persen ~ 10%
+harga_bayar = (1 - 0.3) * total_harga #baris pertama 
+harga_bayar += harga_bayar * pajak # baris kedua
+print("Penyederhanaan kode - harga_bayar=", harga_bayar)
+
+
+
+### TUGAS PRAKTEK ###
+sepatu = { "nama" : "Sepatu Niko", "harga": 150000, "diskon": 30000 }
+baju = { "nama" : "Baju Unikloh", "harga": 80000, "diskon": 8000 }
+celana = { "nama" : "Celana Lepis", "harga": 200000, "diskon": 60000 }
+harga_sepatu = sepatu["harga"] - sepatu["diskon"]
+harga_baju = baju["harga"] - baju["diskon"]
+harga_celana = celana["harga"] - celana["diskon"]
+total_harga = (harga_sepatu + harga_baju + harga_celana) * 1.1 
+print(total_harga)
